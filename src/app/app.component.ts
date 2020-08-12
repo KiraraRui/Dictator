77adf48f4b5f0b884b9082fc461ac79252376915
kira achen <kira.kira@live.dk>
2020-08-12 14:54:08 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder } from '@angular/forms'
+ 
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Dictators';
+export class AppComponent  {
+  regForm: any;
+  data: [object] = [Object];
+  title = 'FormArray Example in Angular Reactive forms';
+  constructor(private formsBuilder: FormBuilder) {
+    this.regForm = this.formsBuilder.group({
+      firstName: '',
+      lastName: '',
+      dateOfBirth: '',
+      dateOfDeath:'',
+      description:''
+    });
+  }
+  
+ 
+  onSubmit(dictator) {
+    console.log(dictator);
+    this.data.push(dictator);
+    console.log(this.data);
+  }
+
+  onRemove() {
+    this.data.splice(this.data.length -1, 1);
+    console.log(this.data);
+  }
+ 
+ 
 }
+
+// this.data.splice(-1, 1);
